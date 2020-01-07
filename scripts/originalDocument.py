fcaf1ae24c1d738507d40b027eab42c96dfc6eff
Lucas Menossi <lucasmenossi03@gmail.com>
2020-01-07 16:48:10 -0300

    Testes iniciais com gráficos

--- scripts/originalDocument.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-#Ler o excel e converter para o formato que o Python pode ler
-xlsx = pd.ExcelFile('PDFpg1Original.xlsx')
-#Converter o excel em um DataFrame
-df = pd.read_excel(xlsx, 'Planilha1')
-#Mostra as colunas com os nomes iniciais e os valores iniciais
-df
